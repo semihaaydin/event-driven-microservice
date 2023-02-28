9d8c9c77c253828b38c7c9ad39e0feed172d577c
Semiha Aydın <semiha.aydinn@gmail.com>
2023-02-28 13:01:41 +0300

    Initial commit

---
+++ src/main/java/com/saydin/ms/eventdrivenmicroservice/EventDrivenMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.saydin.ms.eventdrivenmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventDrivenMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventDrivenMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/saydin/ms/eventdrivenmicroservice/EventDrivenMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saydin.ms.eventdrivenmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventDrivenMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
